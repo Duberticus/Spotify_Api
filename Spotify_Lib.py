eddcd4b382ed0360a37bf1ce26edf836b06cc01c
Duberticus <166752792+Duberticus@users.noreply.github.com>
2025-08-21 21:55:46 -0500

    added things

--- Spotify_Lib.py
+++ Spotify_Lib.py
@@ -1,6 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from spotipy.oauth2 import SpotifyClientCredentials
+
 import os 
 from dotenv import load_dotenv
 
@@ -10,14 +10,21 @@ client_id = os.getenv("client_id")
 secret_id = os.getenv("secret_id")
 print(client_id, secret_id) 
 
-# auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=secret_id)
-# sp = spotipy.Spotify(auth_manager=auth_manager)
+scope = "user-library-read"
+redirect = "http://127.0.0.1:8000/callback"
 
-# playlists = sp.user_playlists('spotify')
-# while playlists:
-#     for i, playlist in enumerate(playlists['items']):
-#         print(f"{i + 1 + playlists['offset']:4d} {playlist['uri']} {playlist['name']}")
-#     if playlists['next']:
-#         playlists = sp.next(playlists)
-#     else:
-#         playlists = None
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,client_secret=secret_id,redirect_uri=redirect,scope=scope))
+
+#max limit is 50 be carful
+# results = sp.current_user_saved_tracks(limit = 50)
+# for idx, item in enumerate(results['items']):
+#     track = item['track']
+#     print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+#practice run
+reading = "playlist-read-public"
+sp_read = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,client_secret=secret_id,redirect_uri=redirect,scope=reading))
+results = sp.current_user_saved_albums(limit = 50)
+for idx, item in enumerate(results['items']):
+    track = item#['track']
+    print(idx, track)#['artists'][0]['name'], " – ", track['name'])
